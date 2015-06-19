9ef662b8a6b48b15b4067b3a17e7aff25198910b
joshandali <joshuahandali@gmail.com>
2015-06-19 16:21:18 +0200

    Added individual RUS and ROS functions. Added balance.multi

--- balance.R
+++ balance.R
@@ -1,42 +1,105 @@
+# perform random undersampling
+# data a dataframe to undersample
+# n number of instances to remove
+# rate undersampling rate
+
+rus <- function(data, n, rate = 0){
+  if(missing(n)) {n = nrow(data)*rate}
+  data = data[-sample(nrow(data), n, replace = F),]
+  
+  return(data)
+}
+
+# perform random oversampling
+# data a dataframe to oversample
+# n number of instances to add
+# rate oversampling rate
+
+ros <- function(data, n, rate = 0){
+  if(missing(n)) {n = nrow(data)*rate}
+  data = rbind(data, data[sample(nrow(data), n, replace = T),])
+  
+  return(data)
+}
+
 # balance the distributions between two classes
 # data a list of dataframes
 # method a selection of methods
+# technique a list of undersampling and oversampling techniques
 # uRate undersampling rate
 # oRate oversampling rate
 # ratio ratio of majority:minority
 
-balance <- function(data, method = c("rus", "ros", "b"), uRate = 0, oRate = 0, ratio = 1) {
+balance <- function(data, method = c("rus", "ros", "b"), technique = list('under' = rus, 'over' = ros), uRate = 0, oRate = 0, ratio = 1){
   method = match.arg(method)
-  rem = nrow(maj)*uRate
-  add = nrow(min)*oRate
+  under = technique$under
+  over = technique$over
   
   # set majority and minority classes
   if(nrow(data[[1]]) >= nrow(data[[2]])){
     maj = data[[1]]
     min = data[[2]]
-  }
-  else {
+  } else {
     maj = data[[2]]
     min = data[[1]]
   }
+  
+  rem = nrow(maj)*uRate
+  add = nrow(min)*oRate
     
   switch(method,
          rus = {
            if(uRate == 0) {rem = nrow(maj) - (ratio*nrow(min))}
-           maj = maj[-sample(nrow(maj), rem, replace = F),]
+           maj = under(maj, rem)
          },
          ros = {
-           if(oRate == 0) {add = nrow(maj) - (ratio*nrow(min))}
-           min = rbind(min, min[sample(nrow(min), add, replace = T),])
+           if(oRate == 0) {add = nrow(maj)/ratio - nrow(min)}
+           min = over(min, add)
          },
          b = {
            if(oRate == 0 || uRate == 0) break
-           rem = nrow(maj)*uRate
-           add = nrow(min)*oRate
-           maj = maj[-sample(nrow(maj), rem, replace = F),]
-           min = rbind(min, min[sample(nrow(min), add, replace = T),])                       
+           maj = under(maj, rate = uRate)
+           min = over(min, rate = oRate)                       
          })
   
   data = list(maj, min)
+  return(data)
+}
+
+# balance multi-class data
+# references: Seliya, N., Xu, Z., and Khoshgoftaar, T. M. Addressing Class Imbalance in Non-Binary Classification Problems. 2008
+# data a list of dataframes
+# method a selection of methods
+# technique a list of undersampling and oversampling techniques
+
+balance.multi <- function(data, method = c("smean", "smedian"), technique = list('under' = rus, 'over' = ros)){
+  method = match.arg(method)
+  under = technique$under
+  over = technique$over
+  
+  switch(method,
+         smean = {
+           m = mean(unlist(lapply(data, nrow)))
+           data = lapply(data, function(x){
+             d = nrow(x) - m
+             if(d > 0){
+               under(x, n = abs(d))
+             } else {
+               over(x, n = abs(d))
+             }
+           })
+         },
+         smedian = {
+           m = median(unlist(lapply(data, nrow)))
+           data = lapply(data, function(x){
+             d = nrow(x) - m
+             if(d > 0){
+               under(x, n = abs(d))
+             } else {
+               over(x, n = abs(d))
+             }
+           })
+         })
+  
   return(data)
 }
