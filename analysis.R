c4bbb9c7976f494e02e5fb6422c0bf36630c91f9
Alexander Anokhin <a_anok01@uni-muenster.de>
2015-06-21 17:25:29 +0200

    update

--- analysis.R
+++ analysis.R
@@ -1,9 +1,186 @@
 setwd("~/Documents/Uni_Munster/DA in Spare Parts Management/fault_diagnosis")
 
 library(ggplot2)
+library(fastICA)
+library(mlr)
+library(kernlab)
+library(pso)
 
 ## Load transformed data
 source("load.R")
 
-data <- rbind(f.1, f.2, f.3, n.1, n.2, n.3)
-plot(data$s1.sd, data$s2.sd, pch=".", col=data$fault)
+## Set number of folds for cross-validation
+folds <- 20
+
+## Part 1
+## Perform visual inspection of classes. Only "Normal 1" is taken into account,
+## because it is only one scenario without load.
+## 
+
+## First plot
+png(filename="images/visualInspection1.png", width = 600, height = 600)
+par(mfcol = c(2, 2))
+plot(faults$s1.sd, faults$s2.sd
+     , pch=ifelse(faults$fault == "Failure 1", "*", ".")
+     , col=ifelse(faults$fault == "Failure 1", "Red", "Blue")
+     , xlab="Std. of signal from sensor 1"
+     , ylab="Std. of signal from sensor 2"
+     , main=paste0("Failure 1 (n = ", weights["Failure 1"], ")"))
+
+plot(faults$s1.sd, faults$s2.sd
+     , pch=ifelse(faults$fault == "Failure 2", "*", ".")
+     , col=ifelse(faults$fault == "Failure 2", "Red", "Blue")
+     , xlab="Std. of signal from sensor 1"
+     , ylab="Std. of signal from sensor 2"
+     , main=paste0("Failure 2 (n = ", weights["Failure 2"], ")"))
+
+plot(faults$s1.sd, faults$s2.sd
+     , pch=ifelse(faults$fault == "Failure 3", "*", ".")
+     , col=ifelse(faults$fault == "Failure 3", "Red", "Blue")
+     , xlab="Std. of signal from sensor 1"
+     , ylab="Std. of signal from sensor 2"
+     , main=paste0("Failure 1 (n = ", weights["Failure 3"], ")"))
+
+plot(faults$s1.sd, faults$s2.sd
+     , pch=ifelse(faults$fault == "Normal 1", "*", ".")
+     , col=ifelse(faults$fault == "Normal 1", "Red", "Blue")
+     , xlab="Std. of signal from sensor 1"
+     , ylab="Std. of signal from sensor 2"
+     , main=paste0("Normal 1 (n = ", weights["Normal 1"], ")"))
+dev.off()
+
+## Second plot
+png(filename="images/visualInspection2.png", width = 600, height = 600)
+par(mfcol = c(2, 2))
+plot(faults$s1.peak, faults$s2.peak
+     , pch=ifelse(faults$fault == "Failure 1", "*", ".")
+     , col=ifelse(faults$fault == "Failure 1", "Red", "Blue")
+     , xlab="Peak of signal from sensor 1"
+     , ylab="Peak of signal from sensor 2"
+     , main=paste0("Failure 1 (n = ", weights["Failure 1"], ")"))
+
+plot(faults$s1.peak, faults$s2.peak
+     , pch=ifelse(faults$fault == "Failure 2", "*", ".")
+     , col=ifelse(faults$fault == "Failure 2", "Red", "Blue")
+     , xlab="Peak of signal from sensor 1"
+     , ylab="Peak of signal from sensor 2"
+     , main=paste0("Failure 2 (n = ", weights["Failure 2"], ")"))
+
+plot(faults$s1.peak, faults$s2.peak
+     , pch=ifelse(faults$fault == "Failure 3", "*", ".")
+     , col=ifelse(faults$fault == "Failure 3", "Red", "Blue")
+     , xlab="Peak of signal from sensor 1"
+     , ylab="Peak of signal from sensor 2"
+     , main=paste0("Failure 1 (n = ", weights["Failure 3"], ")"))
+
+plot(faults$s1.peak, faults$s2.peak
+     , pch=ifelse(faults$fault == "Normal 1", "*", ".")
+     , col=ifelse(faults$fault == "Normal 1", "Red", "Blue")
+     , xlab="Peak of signal from sensor 1"
+     , ylab="Peak of signal from sensor 2"
+     , main=paste0("Normal 1 (n = ", weights["Normal 1"], ")"))
+dev.off()
+
+## Part 2
+## Fit SVM with default settings on pairs of scenarios, then balance 
+## data and compare results (choose best balance technique)
+##
+
+## not balanced fits
+f1.n1 <- rbind(f.1, n.1)
+f1.n1.notbalanced <- ksvm(x=as.matrix(f1.n1[, -c(1, 2, 24)])
+                        , y=f1.n1$fault
+                        , type="C-svc"
+                        , kernel='rbfdot'
+                        , cross = folds)
+
+f2.n1 <- rbind(f.2, n.1)
+f2.n1.notbalanced <- ksvm(x=as.matrix(f2.n1[, -c(1, 2, 24)])
+                          , y=f2.n1$fault
+                          , type="C-svc"
+                          , kernel='rbfdot'
+                          , cross = folds)
+
+f3.n1 <- rbind(f.3, n.1)
+f3.n1.notbalanced <- ksvm(x=as.matrix(f3.n1[, -c(1, 2, 24)])
+                          , y=f3.n1$fault
+                          , type="C-svc"
+                          , kernel='rbfdot'
+                          , cross = folds)
+
+rm(f1.n1); rm(f2.n1); rm(f3.n1)
+
+## TODO: balanced fits
+
+## TODO: comparison
+
+## Part 3
+## Fit SVM with default settings on all classes, then balance/weight
+## data and compare results (choose best balance technique)
+##
+
+## not balanced fits
+fit.notbalanced <- ksvm(x=as.matrix(faults[, -c(1, 2, 24)])
+                          , y=faults$fault
+                          , type="C-svc"
+                          , kernel='rbfdot'
+                          , cross = folds)
+
+## TODO: balanced fits
+
+## TODO: weighted fits Hint: use class.weights parameter
+
+## TODO: comparison
+
+## Part 4
+## Fit SVM with different kernels (maybe implementations) 
+## and compare results (choose best kernel function)
+##
+
+## Part 5
+## Fit k-class SVM and compare results with standard
+## "One-Against-One" approach
+##
+
+## Part 6
+## Explore how feature extraction affects result (PCA analysis, maybe ICA)
+##
+
+## Extract features
+pca.fit <- princomp(faults[, -c(1, 2, 24)], cor=TRUE)
+
+## TODO: explore ICA
+ica.fit <- fastICA(faults[, 3:23], 2)
+
+## Visualise results
+cumsum(pca.fit$sdev^2)/sum(pca.fit$sdev^2)
+plot(x=1:21, cumsum(pca.fit$sdev^2)/sum(pca.fit$sdev^2)
+     , xlab="Number of Components"
+     , ylab="Variance Explained"
+     , type="l"
+     , lwd=2)
+
+## Repeat procedure and find optimal number of components
+
+## Part 7
+## Find best parametrization for SVN using PSO
+##
+
+## Best parametrization
+fc <- function(x) {
+  svp <- ksvm(pca.fit$scores[, 1:17]
+       , faults$fault
+       , type="C-svc"
+       , kernel='rbfdot'
+       , kpar=list("sigma"=.1)
+       , C=x
+       , scaled=c()
+       , cross = 10)
+  error <- attributes(svp)$cross
+  error
+}
+
+optim.c <- psoptim(c(1), fc, lower = 0, upper = 100)
+
+## Predict funal results
+table(predict(svp, pca.fit$scores[, 1:2]), faults$fault) 
--- load.R
+++ load.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(moments)
 
 ## Read data
 load("data/failure_1.RData")
@@ -19,15 +20,28 @@ f.1 <- tbl_df(failure_1) %>%
   group_by(cycle, split) %>%
   summarise(s1.mean = mean(sensor_1)
             , s1.sd = sd(sensor_1)
-            , s1.range = max(sensor_1) - min(sensor_1)
+            , s1.peak = max(sensor_1)
+            , s1.rms = sqrt(mean(sensor_1^2))
+            , s1.cf = max(sensor_1)/sqrt(mean(sensor_1^2))
+            , s1.sk = skewness(sensor_1)
+            , s1.ku = kurtosis(sensor_1)
             , s2.mean = mean(sensor_2)
             , s2.sd = sd(sensor_2)
-            , s2.range = max(sensor_2) - min(sensor_2)
+            , s2.peak = max(sensor_2)
+            , s2.rms = sqrt(mean(sensor_2^2))
+            , s2.cf = max(sensor_2)/sqrt(mean(sensor_2^2))
+            , s2.sk = skewness(sensor_2)
+            , s2.ku = kurtosis(sensor_2)
             , s3.mean = mean(sensor_3)
             , s3.sd = sd(sensor_3)
-            , s3.range = max(sensor_3) - min(sensor_3)) %>%
+            , s3.peak = max(sensor_3)
+            , s3.rms = sqrt(mean(sensor_3^2))
+            , s3.cf = max(sensor_3)/sqrt(mean(sensor_3^2))
+            , s3.sk = skewness(sensor_3)
+            , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
-  mutate(fault = as.factor("Failure 1"))
+  mutate(fault = as.factor("Failure 1")) %>%
+  data.frame()
 
 f.2 <- tbl_df(failure_2) %>%
   group_by(cycle) %>%
@@ -35,15 +49,28 @@ f.2 <- tbl_df(failure_2) %>%
   group_by(cycle, split) %>%
   summarise(s1.mean = mean(sensor_1)
             , s1.sd = sd(sensor_1)
-            , s1.range = max(sensor_1) - min(sensor_1)
+            , s1.peak = max(sensor_1)
+            , s1.rms = sqrt(mean(sensor_1^2))
+            , s1.cf = max(sensor_1)/sqrt(mean(sensor_1^2))
+            , s1.sk = skewness(sensor_1)
+            , s1.ku = kurtosis(sensor_1)
             , s2.mean = mean(sensor_2)
             , s2.sd = sd(sensor_2)
-            , s2.range = max(sensor_2) - min(sensor_2)
+            , s2.peak = max(sensor_2)
+            , s2.rms = sqrt(mean(sensor_2^2))
+            , s2.cf = max(sensor_2)/sqrt(mean(sensor_2^2))
+            , s2.sk = skewness(sensor_2)
+            , s2.ku = kurtosis(sensor_2)
             , s3.mean = mean(sensor_3)
             , s3.sd = sd(sensor_3)
-            , s3.range = max(sensor_3) - min(sensor_3)) %>%
+            , s3.peak = max(sensor_3)
+            , s3.rms = sqrt(mean(sensor_3^2))
+            , s3.cf = max(sensor_3)/sqrt(mean(sensor_3^2))
+            , s3.sk = skewness(sensor_3)
+            , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
-  mutate(fault = as.factor("Failure 2"))
+  mutate(fault = as.factor("Failure 2")) %>%
+  data.frame()
 
 f.3 <- tbl_df(failure_3) %>%
   group_by(cycle) %>%
@@ -51,15 +78,28 @@ f.3 <- tbl_df(failure_3) %>%
   group_by(cycle, split) %>%
   summarise(s1.mean = mean(sensor_1)
             , s1.sd = sd(sensor_1)
-            , s1.range = max(sensor_1) - min(sensor_1)
+            , s1.peak = max(sensor_1)
+            , s1.rms = sqrt(mean(sensor_1^2))
+            , s1.cf = max(sensor_1)/sqrt(mean(sensor_1^2))
+            , s1.sk = skewness(sensor_1)
+            , s1.ku = kurtosis(sensor_1)
             , s2.mean = mean(sensor_2)
             , s2.sd = sd(sensor_2)
-            , s2.range = max(sensor_2) - min(sensor_2)
+            , s2.peak = max(sensor_2)
+            , s2.rms = sqrt(mean(sensor_2^2))
+            , s2.cf = max(sensor_2)/sqrt(mean(sensor_2^2))
+            , s2.sk = skewness(sensor_2)
+            , s2.ku = kurtosis(sensor_2)
             , s3.mean = mean(sensor_3)
             , s3.sd = sd(sensor_3)
-            , s3.range = max(sensor_3) - min(sensor_3)) %>%
+            , s3.peak = max(sensor_3)
+            , s3.rms = sqrt(mean(sensor_3^2))
+            , s3.cf = max(sensor_3)/sqrt(mean(sensor_3^2))
+            , s3.sk = skewness(sensor_3)
+            , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
-  mutate(fault = as.factor("Failure 3"))
+  mutate(fault = as.factor("Failure 3")) %>%
+  data.frame()
 
 n.1 <- tbl_df(normal_1) %>%
   group_by(cycle) %>%
@@ -67,15 +107,28 @@ n.1 <- tbl_df(normal_1) %>%
   group_by(cycle, split) %>%
   summarise(s1.mean = mean(sensor_1)
             , s1.sd = sd(sensor_1)
-            , s1.range = max(sensor_1) - min(sensor_1)
+            , s1.peak = max(sensor_1)
+            , s1.rms = sqrt(mean(sensor_1^2))
+            , s1.cf = max(sensor_1)/sqrt(mean(sensor_1^2))
+            , s1.sk = skewness(sensor_1)
+            , s1.ku = kurtosis(sensor_1)
             , s2.mean = mean(sensor_2)
             , s2.sd = sd(sensor_2)
-            , s2.range = max(sensor_2) - min(sensor_2)
+            , s2.peak = max(sensor_2)
+            , s2.rms = sqrt(mean(sensor_2^2))
+            , s2.cf = max(sensor_2)/sqrt(mean(sensor_2^2))
+            , s2.sk = skewness(sensor_2)
+            , s2.ku = kurtosis(sensor_2)
             , s3.mean = mean(sensor_3)
             , s3.sd = sd(sensor_3)
-            , s3.range = max(sensor_3) - min(sensor_3)) %>%
+            , s3.peak = max(sensor_3)
+            , s3.rms = sqrt(mean(sensor_3^2))
+            , s3.cf = max(sensor_3)/sqrt(mean(sensor_3^2))
+            , s3.sk = skewness(sensor_3)
+            , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
-  mutate(fault = as.factor("Normal 1"))
+  mutate(fault = as.factor("Normal 1")) %>%
+  data.frame()
 
 n.2 <- tbl_df(normal_2) %>%
   group_by(cycle) %>%
@@ -83,15 +136,28 @@ n.2 <- tbl_df(normal_2) %>%
   group_by(cycle, split) %>%
   summarise(s1.mean = mean(sensor_1)
             , s1.sd = sd(sensor_1)
-            , s1.range = max(sensor_1) - min(sensor_1)
+            , s1.peak = max(sensor_1)
+            , s1.rms = sqrt(mean(sensor_1^2))
+            , s1.cf = max(sensor_1)/sqrt(mean(sensor_1^2))
+            , s1.sk = skewness(sensor_1)
+            , s1.ku = kurtosis(sensor_1)
             , s2.mean = mean(sensor_2)
             , s2.sd = sd(sensor_2)
-            , s2.range = max(sensor_2) - min(sensor_2)
+            , s2.peak = max(sensor_2)
+            , s2.rms = sqrt(mean(sensor_2^2))
+            , s2.cf = max(sensor_2)/sqrt(mean(sensor_2^2))
+            , s2.sk = skewness(sensor_2)
+            , s2.ku = kurtosis(sensor_2)
             , s3.mean = mean(sensor_3)
             , s3.sd = sd(sensor_3)
-            , s3.range = max(sensor_3) - min(sensor_3)) %>%
+            , s3.peak = max(sensor_3)
+            , s3.rms = sqrt(mean(sensor_3^2))
+            , s3.cf = max(sensor_3)/sqrt(mean(sensor_3^2))
+            , s3.sk = skewness(sensor_3)
+            , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
-  mutate(fault = as.factor("Normal 2"))
+  mutate(fault = as.factor("Normal 2")) %>%
+  data.frame()
 
 n.3 <- tbl_df(normal_3) %>%
   group_by(cycle) %>%
@@ -99,19 +165,34 @@ n.3 <- tbl_df(normal_3) %>%
   group_by(cycle, split) %>%
   summarise(s1.mean = mean(sensor_1)
             , s1.sd = sd(sensor_1)
-            , s1.range = max(sensor_1) - min(sensor_1)
+            , s1.peak = max(sensor_1)
+            , s1.rms = sqrt(mean(sensor_1^2))
+            , s1.cf = max(sensor_1)/sqrt(mean(sensor_1^2))
+            , s1.sk = skewness(sensor_1)
+            , s1.ku = kurtosis(sensor_1)
             , s2.mean = mean(sensor_2)
             , s2.sd = sd(sensor_2)
-            , s2.range = max(sensor_2) - min(sensor_2)
+            , s2.peak = max(sensor_2)
+            , s2.rms = sqrt(mean(sensor_2^2))
+            , s2.cf = max(sensor_2)/sqrt(mean(sensor_2^2))
+            , s2.sk = skewness(sensor_2)
+            , s2.ku = kurtosis(sensor_2)
             , s3.mean = mean(sensor_3)
             , s3.sd = sd(sensor_3)
-            , s3.range = max(sensor_3) - min(sensor_3)) %>%
+            , s3.peak = max(sensor_3)
+            , s3.rms = sqrt(mean(sensor_3^2))
+            , s3.cf = max(sensor_3)/sqrt(mean(sensor_3^2))
+            , s3.sk = skewness(sensor_3)
+            , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
-  mutate(fault = as.factor("Normal 3"))
+  mutate(fault = as.factor("Normal 3")) %>%
+  data.frame()
 
-rm(failure_1)
-rm(failure_2)
-rm(failure_3)
-rm(normal_1)
-rm(normal_2)
-rm(normal_3)
+rm(failure_1); rm(failure_2); rm(failure_3)
+rm(normal_1); rm(normal_2); rm(normal_3)
+
+## Combine all data together
+faults <- rbind(f.1, f.2, f.3, n.1, n.2, n.3)
+
+## Calulate weights of classes
+weights <- table(faults$fault)
