4176b00424cb19099bca9cc35b44537544b15efb
Alexander Anokhin <a_anok01@uni-muenster.de>
2015-06-22 16:31:00 +0200

    filter stable process

--- load.R
+++ load.R
@@ -13,6 +13,9 @@ load("data/normal_3.RData")
 ## How many seconds in one split
 seconds <- 5
 
+## Persentage to be considered
+q <- .05
+
 ## Transform data
 f.1 <- tbl_df(failure_1) %>%
   group_by(cycle) %>%
@@ -41,6 +44,9 @@ f.1 <- tbl_df(failure_1) %>%
             , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
   mutate(fault = as.factor("Failure 1")) %>%
+  group_by(cycle) %>%
+  ## filter splits with stable signal
+  filter(s1.sd >= quantile(s1.sd, q)) %>%
   data.frame()
 
 f.2 <- tbl_df(failure_2) %>%
@@ -70,6 +76,9 @@ f.2 <- tbl_df(failure_2) %>%
             , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
   mutate(fault = as.factor("Failure 2")) %>%
+  group_by(cycle) %>%
+  ## filter splits with stable signal
+  filter(s1.sd >= quantile(s1.sd, q)) %>%
   data.frame()
 
 f.3 <- tbl_df(failure_3) %>%
@@ -99,6 +108,9 @@ f.3 <- tbl_df(failure_3) %>%
             , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
   mutate(fault = as.factor("Failure 3")) %>%
+  group_by(cycle) %>%
+  ## filter splits with stable signal
+  filter(s1.sd >= quantile(s1.sd, q)) %>%
   data.frame()
 
 n.1 <- tbl_df(normal_1) %>%
@@ -128,6 +140,9 @@ n.1 <- tbl_df(normal_1) %>%
             , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
   mutate(fault = as.factor("Normal 1")) %>%
+  group_by(cycle) %>%
+  ## filter splits with stable signal
+  filter(s1.sd >= quantile(s1.sd, q)) %>%
   data.frame()
 
 n.2 <- tbl_df(normal_2) %>%
@@ -157,6 +172,9 @@ n.2 <- tbl_df(normal_2) %>%
             , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
   mutate(fault = as.factor("Normal 2")) %>%
+  group_by(cycle) %>%
+  ## filter splits with stable signal
+  filter(s1.sd >= quantile(s1.sd, q)) %>%
   data.frame()
 
 n.3 <- tbl_df(normal_3) %>%
@@ -186,6 +204,9 @@ n.3 <- tbl_df(normal_3) %>%
             , s3.ku = kurtosis(sensor_3)) %>%
   filter(complete.cases(s1.sd, s2.sd, s3.sd)) %>%
   mutate(fault = as.factor("Normal 3")) %>%
+  group_by(cycle) %>%
+  ## filter splits with stable signal
+  filter(s1.sd >= quantile(s1.sd, q)) %>%
   data.frame()
 
 rm(failure_1); rm(failure_2); rm(failure_3)
