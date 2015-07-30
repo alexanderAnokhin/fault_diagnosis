ad1accc4be2e68c0d47dc4320d6004cd4181eedb
joshandali <joshuahandali@gmail.com>
2015-07-30 22:46:29 +0200

    remove helper.R

--- resources/helper.R
+++
@@ -1,9 +0,0 @@
-ksearch <- function(data, nc=15){
-  wss <- (nrow(data)-1)*sum(apply(data,2,var))
-  for (i in 2:nc){
-    wss[i] <- sum(kmeans(data, centers=i)$withinss)}
-  diff2 <- diff(abs(diff(wss)))
-  res <- which(abs(diff2) == max(abs(diff2))) + 1
-  
-  return(res)
-}
